c06188063682675fbd6701137652929a9d47d11e
weida <752087756@qq.com>
2020-10-07 21:22:14 +0800

    tools

---
+++ src/leetcode/medium/easy145_postorderTraversal.java
@@ -0,0 +1,71 @@
+package pers.weida.leetcode.medium;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Stack;
+
+import pers.weida.leetcode.tools.TreeNode;
+
+/**
+ * 145. 二叉树的后序遍历
+ * 给定一个二叉树，返回它的 后序 遍历。
+ *
+ * 示例:
+ *
+ * 输入: [1,null,2,3]
+ *    1
+ *     \
+ *      2
+ *     /
+ *    3
+ *sss
+ * 输出: [3,2,1]
+ * 进阶: 递归算法很简单，你可以通过迭代算法完成吗？
+ *
+ *
+ * kljlkjlssss三四三sss 三四三履历ssssssssaaa
+ */
+
+
+
+public class easy145_postorderTraversal {
+    //version2: iterate----------------------------------------------------
+    public static List<Integer> iterate(TreeNode root) {
+        LinkedList<Integer> list = new LinkedList<>();
+        Stack<TreeNode> stack = new Stack();
+        while (!stack.empty() || root != null) {
+            while (root != null) {
+                list.addFirst(root.val);
+                stack.push(root);
+                root = root.right;
+            }
+            if (!stack.empty()) {
+                root = stack.pop();
+                root = root.left;
+            }
+        }
+        return list;
+    }
+
+    //version1: traversal----------------------------------------------------
+    public static List<Integer> postorderTraversal(TreeNode root) {
+        List<Integer> res = new ArrayList<Integer>();
+        postorder(root, res);
+        return res;
+    }
+    public static void postorder(TreeNode root, List<Integer> res){
+        if (root == null){
+            return;
+        }
+        postorder(root.left, res);
+        postorder(root.right, res);
+        res.add(root.val);
+    }
+
+    public static void main(String[] args) {
+        Integer[] integers = {1, null, 2, 3};
+        TreeNode treeNode = TreeNode.InitTree(integers);
+        List res = iterate(treeNode);
+        System.out.println(res);
+    }
+}
---
+++ src/leetcode/medium/easy283_MoveZeroes.java
@@ -0,0 +1,35 @@
+package pers.weida.leetcode.medium;
+
+import pers.weida.leetcode.tools.TreeNode;
+
+import java.util.ArrayList;
+import java.util.List;
+import pers.weida.leetcode.tools.Weida_utils;
+
+/**
+ * Given an array nums, write a function to move all 0's to the end of it while maintaining the relative order of the non-zero elements.
+ *
+ * Example:
+ *
+ * Input: [0,1,0,3,12]
+ * Output: [1,3,12,0,0]
+ */
+
+public class easy283_MoveZeroes {
+    public void moveZeroes(int[] nums) {
+        if (nums == null || nums.length == 0)
+            return;
+        int insertPos=0;
+        for (int num: nums){
+            if (num != 0)   nums[insertPos++] = num;
+        }
+        while (insertPos < nums.length)
+            nums[insertPos++]=0;
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {0,1,0,3,12};
+        new easy283_MoveZeroes().moveZeroes(nums);
+        Weida_utils.print_arr(nums);
+    }
+}
