0b780f030fc97809c50780c553eb19adbeaeac54
weida <752087756@qq.com>
2020-10-09 08:32:15 +0800

    383,387

--- src/easy/easy383.java
+++ src/easy/easy383.java
@@ -1,2 +1,91 @@
-package easy;public class easy383 {
+package easy;
+
+public class easy383 {
+    /**
+     *
+     * @param ransomNote
+     * @param magazine
+     * @return
+     * 执行用时：
+     * 1 ms
+     * , 在所有 Java 提交中击败了
+     * 100.00%
+     * 的用户
+     * 内存消耗：
+     * 39.2 MB
+     * , 在所有 Java 提交中击败了
+     * 58.88%
+     * 的用户
+     */
+    public static boolean canConstructv2(String ransomNote, String magazine) {
+        if (ransomNote.length() > magazine.length())
+            return false;
+        int[] begin_index = new int[26];
+        for (int i=0; i<ransomNote.length(); i++){
+            char c = ransomNote.charAt(i);
+            int index = magazine.indexOf(c, begin_index[c-97]);
+            if (index < 0)
+                return false;
+            else{
+                begin_index[c-97] = index+1;
+            }
+        }
+        return true;
+    }
+
+    /**'
+     *
+     * @param ransomNote
+     * @param magazine
+     * @return
+     * 执行用时：
+     * 2 ms
+     * , 在所有 Java 提交中击败了
+     * 94.08%
+     * 的用户
+     * 内存消耗：
+     * 39.2 MB
+     * , 在所有 Java 提交中击败了
+     * 48.87%
+     * 的用户
+     * 炫耀一下:
+     */
+    public static boolean canConstruct(String ransomNote, String magazine) {
+        int[] map = new int[26];
+//        System.out.println(map[1]);
+        for (int i = 0; i < magazine.length(); i++){
+            map[magazine.charAt(i) - 97]++;
+        }
+        for(int i=0; i<ransomNote.length();i++){
+            if(map[ransomNote.charAt(i)-97] > 0){
+                map[ransomNote.charAt(i)-97]--;
+            }else{
+                return false;
+            }
+        }
+        return true;
+    }
+
+    //error: 超出输出限制
+    public static boolean canConstructv1(String ransomNote, String magazine) {
+        if (ransomNote.length() == 0)
+            return true;
+        String filt = ransomNote.substring(0, 1);
+        int index = magazine.indexOf(filt);
+        String filted_str="";
+        if (index < 0)
+            return false;
+        else if (index==0) {
+            filted_str = magazine.substring(index+1);
+        } else{
+            filted_str = magazine.substring(0, index)+ magazine.substring(index+1);
+        }
+        System.out.println(filted_str);
+        return canConstruct(ransomNote.substring(1),filted_str);
+
+    }
+    public  static void main(String[] args) {
+        boolean res = canConstructv2("aa", "aba");
+        System.out.print(res);
+    }
 }
--- src/easy/easy387.java
+++ src/easy/easy387.java
@@ -1,2 +1,82 @@
-package easy;public class easy387 {
+package easy;
+
+import java.util.HashMap;
+
+/**
+ * 387. 字符串中的第一个唯一字符
+ * 给定一个字符串，找到它的第一个不重复的字符，并返回它的索引。如果不存在，则返回 -1。
+ *
+ *
+ *
+ * 示例：
+ *
+ * s = "leetcode"
+ * 返回 0
+ *
+ * s = "loveleetcode"
+ * 返回 2
+ */
+public class easy387 {
+
+    /**
+     * 执行用时：
+     * 6 ms
+     * , 在所有 Java 提交中击败了
+     * 88.82%
+     * 的用户
+     * 内存消耗：
+     * 39.2 MB
+     * , 在所有 Java 提交中击败了
+     * 82.90%
+     * 的用户
+     * @param s
+     * @return
+     */
+    public static int firstUniqCharv2(String s) {
+        int[] count = new int[26];
+        //build
+        int n = s.length();
+        for (int i=0; i<n; i++){
+            char c = s.charAt(i);
+            count[c-97]++;
+        }
+        for (int i=0; i<n; i++){
+            char c = s.charAt(i);
+            if (count[c-97] == 1)
+                return i;
+        }
+        return -1;
+    }
+    /**
+     * 执行用时：
+     * 33 ms
+     * , 在所有 Java 提交中击败了
+     * 52.14%
+     * 的用户
+     * 内存消耗：
+     * 39.6 MB
+     * , 在所有 Java 提交中击败了
+     * 25.22%
+     * 的用户
+     * @param s
+     * @return
+     */
+    public static int firstUniqChar(String s) {
+        HashMap<Character, Integer> count = new HashMap<Character, Integer>();
+        int n = s.length();
+        //build hashMap
+        for (int i=0; i<n; i++){
+            char c = s.charAt(i);
+            count.put(c, count.getOrDefault(c, 0)+1);
+        }
+        for (int i=0; i<n; i++){
+            if (count.get(s.charAt(i)) == 1)
+                return i;
+        }
+        return -1;
+    }
+    public  static void main(String[] args) {
+        int res = firstUniqCharv2("khksahdkvnba");
+        System.out.print(res);
+    }
 }
