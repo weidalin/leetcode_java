c24a60c77b24290a72e188165206dfb1c4e53bbd
weidalin <752087756@qq.com>
2021-07-12 16:42:37 +0800

    20210712

--- src/leetcode/swordoffer/offer16_myPow/myPow_2.java
+++ src/leetcode/swordoffer/offer16_myPow/myPow_2.java
@@ -18,17 +18,23 @@ package leetcode.swordoffer.offer16_myPow;
  * 输出：0.25000
  * 解释：2-2 = 1/22 = 1/4 = 0.25
  */
-public class myPow_1 {
-    public static double myPow(double x, int n) {
-        if(n == 0){
-            return 1;
-        }else if(n < 0){
-            return 1/(x * myPow(x, -n-1));
-//            return 1/x * myPow(x, -n-1);
-        }else if(n % 2 == 1){
-            return x * myPow(x, n - 1);
-        }else{
-            return myPow(x * x, n / 2);
+public class myPow_2 {
+    public double myPow(double x, int n) {
+        if(x == 0)
+            return 0;
+        long b = n;
+        double res = 1.0;
+        if(b < 0){
+            x =  1 / x;
+            b = -b;
         }
+        while(b > 0){
+            if((b & 1) == 1){
+                res *= x;
+            }
+            x *= x;
+            b >>= 1;
+        }
+        return res;
     }
 }
