b72a1cb938fdab0a5a09cb87aa605827e9d4601f
weidalin <752087756@qq.com>
2021-07-28 19:11:46 +0800

    fished slided window

--- src/leetcode/sliding_window/offer30_findSubstring/findSubstring_2.java
+++ src/leetcode/sliding_window/offer30_findSubstring/findSubstring_2.java
@@ -3,14 +3,15 @@ package leetcode.sliding_window.offer30_findSubstring;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
 
-public class findSubstring_1 {
+public class findSubstring_2 {
     /**
      * 30. 串联所有单词的子串
      * 给定一个字符串 s 和一些 长度相同 的单词 words 。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
      *
      * 注意子串要与 words 中的单词完全匹配，中间不能有其他字符 ，但不需要考虑 words 中单词串联的顺序。
+     *
+     *
      *
      * 示例 1：
      *
@@ -33,30 +34,22 @@ public class findSubstring_1 {
      */
     public List<Integer> findSubstring(String s, String[] words) {
         List<Integer> res = new ArrayList<>();
-        if(s == null || s.length() < words.length || words == null || words.length == 0)    return res;
-        Map<String, Integer> map = new HashMap<>();
+        if(s == null || s.length() == 0 || words == null || words.length == 0)  return res;
+        HashMap<String, Integer> map = new HashMap<>();
+        int one_word = words[0].length();
+        int word_num = words.length;
+        int all_len = one_word * word_num;
         for(String word : words){
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
-        int one_word = words[0].length();
-        int len = one_word & words.length;
-        for(int i = 0; i < one_word; i++){
-            int left = i, right = i, count = 0;
-
-            Map<String, Integer> tmp_map = new HashMap<>();
-            while(right + one_word <= s.length()){
-                String w = s.substring(right, right + one_word);
+        for(int i = 0; i < s.length() - all_len + 1; i++){
+            String tmp = s.substring(i, i + all_len);
+            HashMap<String, Integer> tmp_map = new HashMap<>();
+            for(int j = 0; j < all_len; j+=one_word){
+                String w = tmp.substring(j, j + one_word);
                 tmp_map.put(w, tmp_map.getOrDefault(w, 0) + 1);
-                right += one_word;
-                count++;
-                while (tmp_map.getOrDefault(w, 0) > map.getOrDefault(w, 0)) {
-                    String t_w = s.substring(left, left + one_word);
-                    count--;
-                    tmp_map.put(t_w, tmp_map.getOrDefault(t_w, 0) - 1);
-                    left += one_word;
-                }
-                if (count == words.length) res.add(left);
             }
+            if(map.equals(tmp_map)) res.add(i);
         }
         return res;
     }
--- src/leetcode/sliding_window/offer30_findSubstring/findSubstring_3.java
+++ src/leetcode/sliding_window/offer30_findSubstring/findSubstring_3.java
@@ -5,12 +5,14 @@ import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
-public class findSubstring_1 {
+public class findSubstring_3 {
     /**
      * 30. 串联所有单词的子串
      * 给定一个字符串 s 和一些 长度相同 的单词 words 。找出 s 中恰好可以由 words 中所有单词串联形成的子串的起始位置。
      *
      * 注意子串要与 words 中的单词完全匹配，中间不能有其他字符 ，但不需要考虑 words 中单词串联的顺序。
+     *
+     *
      *
      * 示例 1：
      *
@@ -31,7 +33,7 @@ public class findSubstring_1 {
      * @param words
      * @return
      */
-    public List<Integer> findSubstring(String s, String[] words) {
+    public static List<Integer> findSubstring(String s, String[] words) {
         List<Integer> res = new ArrayList<>();
         if(s == null || s.length() < words.length || words == null || words.length == 0)    return res;
         Map<String, Integer> map = new HashMap<>();
@@ -39,7 +41,6 @@ public class findSubstring_1 {
             map.put(word, map.getOrDefault(word, 0) + 1);
         }
         int one_word = words[0].length();
-        int len = one_word & words.length;
         for(int i = 0; i < one_word; i++){
             int left = i, right = i, count = 0;
 
@@ -47,17 +48,27 @@ public class findSubstring_1 {
             while(right + one_word <= s.length()){
                 String w = s.substring(right, right + one_word);
                 tmp_map.put(w, tmp_map.getOrDefault(w, 0) + 1);
+                count ++;
                 right += one_word;
-                count++;
-                while (tmp_map.getOrDefault(w, 0) > map.getOrDefault(w, 0)) {
+
+                while(map.getOrDefault(w, 0) < tmp_map.getOrDefault(w, 0)){
                     String t_w = s.substring(left, left + one_word);
-                    count--;
                     tmp_map.put(t_w, tmp_map.getOrDefault(t_w, 0) - 1);
                     left += one_word;
+                    count --;
+                    // System.out.println(count +"--=--"+tmp_map + " ---=--- " + map);
                 }
-                if (count == words.length) res.add(left);
+                if(count == words.length)
+                    res.add(left);
             }
         }
         return res;
+    }
+
+    public static void main(String[] args) {
+        String s = "wordgoodgoodgoodbestword";
+        String[] word = {"word","good","best","word"};
+        List<Integer> res = findSubstring(s, word);
+        System.out.println(res);
     }
 }
