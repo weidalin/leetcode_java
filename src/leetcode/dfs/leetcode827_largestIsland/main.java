66fd213a52140834ef7a8519ddf3aed5b6a07f8f
weidalin <752087756@qq.com>
2021-08-09 19:37:07 +0800

    2021-08-09

--- src/leetcode/dfs/leetcode827_largestIsland/main.java
+++ src/leetcode/dfs/leetcode827_largestIsland/main.java
@@ -23,9 +23,8 @@ public class main {
         int[][] matrix = {  {1,3,1},
                             {1,5,1},
                             {4,2,1}};
-        int[][] matrix2 = {{1,1,0},
-                            {0,0,1},
-                            {1,1,1}};
+        int[][] matrix2 = {{1,1,1},
+                            {0,0,1}};
         int[][] matrix3 = {{1,2,3,4},{5,6,7,8},{9,10,11,12}};
 
         Integer[] tree_input_1 = {4,1,6,0,2,5,7,null,null,null,3,null,null,null,8};
