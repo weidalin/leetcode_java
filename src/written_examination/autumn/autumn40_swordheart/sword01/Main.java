93e393cc47b17676ef156bf1a1af848028036dfd
weidalin <752087756@qq.com>
2021-10-27 16:46:38 +0800

    1027

---
+++ src/written_examination/autumn/autumn40_swordheart/sword01/Main.java
@@ -0,0 +1,12 @@
+package written_examination.autumn.autumn40_swordheart.sword01;
+
+public class Main {
+    /*
+    <div class="subject-question"><div>  小强同学背着一筐数量为N的石头（每块石头重量相同），每走一步可以从以下方案中选择一种 </div> <div>  方案1：如果当前框里石头为2的倍数，那么可以丢掉其中的1/2； </div> <div>  方案2：<span>如果<span>当前</span>框里石头为3的倍数，</span>丢掉框里<span>所剩石头的</span>的2/3； </div> <div>  <span>方案3：如果<span>当前</span>框里的石头不是2或者3的倍数，那么只能丢一块石头；</span><br> </div> <div>  <div>   注意只能把石头整个丢掉，问小强最少走多少步才可以把石头全丢完  </div> </div></div>
+    <div><h2 class="fs strong">输入描述:</h2> <pre>开始时框里石头的数量</pre> <h2 class="fs strong">输出描述:</h2> <pre>只需要输出最少走的步数，不需要输出过程</pre></div>
+    10
+    4
+    <pre><div>在此样例中，每步丢掉的石头数量以及剩余数量对应如下：</div><div>丢掉的数量&nbsp; &nbsp; 剩余数量</div><div>&nbsp; &nbsp;1&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;9</div><div>&nbsp; &nbsp;6&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;3</div><div>&nbsp; &nbsp;2&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;1</div><div>&nbsp; &nbsp;1&nbsp; &nbsp; &nbsp; &nbsp; &nbsp; &nbsp;0</div><div>一共需要4步才可以把石头全部丢掉</div><div>最终只需要输出步数，不需要输出丢弃过程</div></pre>
+    <pre><div>评分标准：</div><div>差：暴力遍历</div><div>中：动态规划</div><div>好：动态规划+贪心算法</div></pre>
+     */
+}
---
+++ src/written_examination/autumn/autumn40_swordheart/sword03/Main.java
@@ -0,0 +1,9 @@
+package written_examination.autumn.autumn40_swordheart.sword03;
+
+public class Main {
+    /*<div class="subject-question"><div>  <span style="color: rgb(77,77,77);">A, B 两位玩家在做一个数字拾取游戏, 给定一个正整数数列S，S的长度必定是偶数, A先从S首尾选一个数字，然后B从剩下数字首尾选一个数字，假设A,B都足够聪明，以保证自己拿到的最终分数最高(拾取的数字和最高).</span>  </div> <div>  <span style="color: rgb(77,77,77);">给定这个数列S,求A,B的最终得分.</span>  </div> <div>  <span style="color: rgb(51,51,51);"></span>  </div></div>
+    [3,5,2,1]
+    [6,5]
+
+     */
+}
